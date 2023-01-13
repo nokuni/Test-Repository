14cab302aa4640141bdded7bbe8e0bf101e6ea18
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-01-14 00:31:56 +0100

    Initial Commit

---
+++ TestRepoProject/TestRepoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoProjectApp.swift
+//  TestRepoProject
+//
+//  Created by Maertens Yann-Christophe on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
